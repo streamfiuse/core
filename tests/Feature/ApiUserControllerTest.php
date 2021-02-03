244d9c3de939d56cbb8f49b3d0a5ab653033e9e7
finbiessler <fin.biess@gmx.de>
2021-02-03 23:08:29 +0100

    WIP

--- tests/Feature/ApiUserControllerTest.php
+++ tests/Feature/ApiUserControllerTest.php
@@ -10,6 +10,14 @@ class ApiUserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    private string $baseUrl;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->baseUrl =  env('APP_URL') . '/api';
+    }
+
     public function provideRegisterCreatesNewUserData(): array
     {
         return [
@@ -60,12 +68,88 @@ class ApiUserControllerTest extends TestCase
 
     /**
      * @dataProvider provideRegisterCreatesNewUserData
+     * @param array $expectedResult
+     * @param array $input
      */
-    public function testRegisterCreatesNewUser($expectedResult, $input): void
+    public function testRegisterCreatesNewUser(array $expectedResult, array $input): void
     {
-        // build base url for core-api requests
-        $baseUrl = env('APP_URL') . '/api';
-
-        self::assertEquals(json_encode($expectedResult), Http::post($baseUrl . '/register-api-user', $input)->body());
+        self::assertEquals(json_encode($expectedResult), Http::post($this->baseUrl . '/register-api-user', $input)->body());
     }
+
+    public function provideLoginAuthenticatesNewUserData(): array
+    {
+        return [
+            'Correct Input' => [
+              [
+                  'status' => 'success',
+                  'data' => [
+                      'name' => 'tester',
+                      'email' => 'tester@mail.com'
+                  ]
+              ],
+              [
+                  'name' => 'tester',
+                  'email' => 'tester@email.com',
+                  'password' => 'test',
+              ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideLoginAuthenticatesNewUserData()
+     * @param array $expectedResult
+     * @param array $input
+     */
+    public function testCorrectLoginAuthenticatesNewUser(array $expectedResult, array $input): void
+    {
+        Http::post($this->baseUrl . '/register-api-user', $input);
+        $bearerToken = Http::post($this->baseUrl . '/login-api-user' , [
+            'email' => 'tester@mail.com',
+            'password' => 'test'])->json('token');
+
+        $expectedResponse = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $bearerToken
+        ])->get($this->baseUrl . '/logged-in-user')->body();
+
+        self::assertEquals(json_encode($expectedResult), $expectedResponse);
+    }
+
+    //TODO test with wrong types of input, left off parameters, wrong pw etc.
+
+    public function provideLogoutLogsOutUserWithCorrectToken(): array
+    {
+        return [
+            'Correct Input' => [
+                405,
+                [
+                    'name' => 'tester',
+                    'email' => 'tester@email.com',
+                    'password' => 'test',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideLogoutLogsOutUserWithCorrectToken
+     * @param $expectedResult
+     * @param $input
+     */
+    public function testLogoutLogsOutUserWithCorrectToken($expectedResult, $input): void
+    {
+        Http::post($this->baseUrl . '/register-api-user', $input);
+        $bearerToken = Http::post($this->baseUrl . '/login-api-user' , [
+            'email' => 'tester@mail.com',
+            'password' => 'test'])->json('token');
+        Http::withHeaders([
+            'Authorization' => 'Bearer ' . $bearerToken
+        ])->post($this->baseUrl . '/logout-api-user');
+        $actualHttpStatus = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $bearerToken
+        ])->get($this->baseUrl . '/logged-in-user')->status();
+        self::assertEquals($expectedResult, $actualHttpStatus);
+    }
+
+    //TODO Test behaviour when no user is logged in
 }
