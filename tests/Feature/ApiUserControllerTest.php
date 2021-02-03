74116943de1704bf8e8e7036eaacd2dea7a4ac22
finbiessler <fin.biess@gmx.de>
2021-02-03 17:26:31 +0100

    - refactored first test case such that it tests all testable cases

--- tests/Feature/ApiUserControllerTest.php
+++ tests/Feature/ApiUserControllerTest.php
@@ -18,24 +18,67 @@ class ApiUserControllerTest extends TestCase
         // build base url for core-api requests
         $baseUrl = env('APP_URL') . '/api';
 
-        $expectedData = [
-            'status' => 'success',
-            'name' => 'tester',
-            'email' => 'tester@mail.com',
+        // Prepare different input data sets
+        $inputData = [
+            0 => [
+                'name' => 'tester',
+                'password' => 'test'
+            ],
+
+            1 => [
+              'name' => 'tester',
+              'email' => 'myemail',
+              'password' => 'test'
+            ],
+
+             2 => [
+                'name' => 'tester',
+                'email' => 'tester@mail.com',
+                'password' => 'test'
+            ]
         ];
 
-        // get actual response by post requesting the api
-        $actualResponse = Http::post($baseUrl . '/register-api-user', [
-           'name' => 'tester',
-           'email' => 'tester@mail.com',
-           'password' => 'test'
-        ]);
+        // Prepare the respective expected data
+        $expectedData = [
+            0 => [
+                'status' => 'failed',
+                'name' => null,
+                'email' => null,
+                'http_status' => '422'
+            ],
+            1 => [
+                'status' => 'failed',
+                'name' => null,
+                'email' => null,
+                'http_status' => '422'
+            ],
+            2 => [
+                'status' => 'success',
+                'name' => 'tester',
+                'email' => 'tester@mail.com',
+                'http_status' => 201
+            ]
+        ];
 
-        // prepare response for assertion i.e. extract the data which is to be asserted
-        $actualData['status'] = $actualResponse->json('status');
-        $actualData['name'] = $actualResponse->json('data')['name'];
-        $actualData['email'] = $actualResponse->json('data')['email'];
+        // for each $input in $inputData fire a api request to the /register-api-user endpoint and retrieve the data
+        $index = 0;
+        foreach ($inputData as $message => $input) {
+            $actualResponse = Http::post($baseUrl . '/register-api-user', $input);
 
-        self::assertEquals($expectedData, $actualData);
+            // purify the data in the response in the $actualData array
+            $actualData['status'] = $actualResponse->json('status');
+            if ($index === 2) {
+                $actualData['name'] = $actualResponse->json('data')['name'];
+                $actualData['email'] = $actualResponse->json('data')['email'];
+            } else {
+                $actualData['name'] = null;
+                $actualData['email'] = null;
+            }
+            $actualData['http_status'] = (string) $actualResponse->status();
+
+            // assert that the corresponding expectedData and actualData are the same
+            self::assertEquals($expectedData[$index], $actualData);
+            $index++;
+        }
     }
 }
