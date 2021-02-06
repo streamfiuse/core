b086582e3193f7aca10d52532134e9c996b9988e
finbiessler <fin.biess@gmx.de>
2021-02-06 22:59:16 +0100

    - Created DatabaseSeeder for Production to create a apiuser the other TestDatabaseSeeder should not be run on prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            'name' => 'apiuser',
+            'email' => 'apiuser@mail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$8KDbZks3XqWTsR0Fbo31E.f.mgJgEF/94e7/uUHQw7SAGugR1lA/q', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
