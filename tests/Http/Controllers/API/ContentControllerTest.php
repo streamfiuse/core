6e8e8b519b0d281298da4412876b66f65209fa11
finbiessler <fin.biess@gmx.de>
2021-02-06 22:59:23 +0100

    - WIP

--- tests/Http/Controllers/API/ContentControllerTest.php
+++ tests/Http/Controllers/API/ContentControllerTest.php
@@ -3,13 +3,20 @@
 namespace Tests\Http\Controllers\API;
 
 use App\Http\Controllers\API\ContentController;
+use App\Models\Content;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class ContentControllerTest extends TestCase
 {
 
     use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Content::factory()->count(100)->create();
+    }
 
     public function testIndexReturnsAllContents(): void
     {
