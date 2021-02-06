8aff49aba2692cae8cdc602d2783070925a9268b
finbiessler <fin.biess@gmx.de>
2021-02-06 18:22:12 +0100

    WIP

---
+++ tests/Http/Controllers/API/ContentControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Http\Controllers\API;
+
+use App\Http\Controllers\API\ContentController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class ContentControllerTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function testIndexReturnsAllContents(): void
+    {
+
+    }
+}
