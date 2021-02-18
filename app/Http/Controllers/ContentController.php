733411a9416057f7a3ae57f893ac0ab77494a022
finbiessler <fin.biess@gmx.de>
2021-02-18 18:54:31 +0100

    WIP TDD of content crud api

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -2,20 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ContentResource;
 use App\Models\Content;
-use http\Env\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ContentController extends Controller
 {
     public function index(): JsonResponse
     {
+        $contents = Content::all();
 
+        return response()->json([
+            'status' => 'success',
+            'data' => ContentResource::collection($contents)
+        ], 200);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|unique:content,title',
+            'release_date' => 'required|date_format:Y-m-d',
+            'content_type' => 'required|string',
+            'genre' => 'required|json',
+            'tags' => 'required|json',
+            'runtime' => 'required|integer|gt:0',
+            'short_description' => 'required|string',
+            'cast' => 'required|json',
+            'directors' => 'required|json',
+            'age_restriction' => 'required|string',
+            'poster_url' => 'required|url',
+            'youtube_trailer_url' => 'required|url',
+            'production_company' => 'required|string',
+            'seasons' => 'required|integer|gt:0',
+            'average_episode_count' => 'required|integer|gt:0',
+        ]);
+
+        // Validate
+        if ($validator->fails()) {
+
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid input!',
+                'validation_errors' => $validator->errors()
+            ],
+                422);
+
+        }
+
+        // Create content with the input given in the request
+        $content = Content::create($request->all());
+
+        // Check whether the creation was successful
+        if (!is_null($content)) {
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Successfully created a new content entry',
+                'content_created' => $content],
+                201);
+
+        }
+
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Unable to create new content entry'
+        ],
+            500);
 
     }
 
--- tests/Http/Controllers/Api/ContentControllerTest.php
+++ tests/Http/Controllers/Api/ContentControllerTest.php
@@ -3,26 +3,79 @@
 namespace Tests\Http\Controllers\Api;
 
 use App\Http\Controllers\ContentController;
+use App\Models\Content;
+use App\Models\User;
+use App\Traits\ApiUserAuthenticationTrait;
+use Database\Factories\ContentFactory;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ContentControllerTest extends TestCase
 {
+    private string $baseUrl;
+    private User $user;
+
+    const CONTENT_COUNT = 100;
+
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->postJson('/api/register-api-user', [
-            'name' => 'tester',
-            'email' => 'tester@mail.com',
-            'password' => 'test',
-            'master_password' => 'lufin0205'
-        ]);
+        $this->baseUrl = env('APP_URL') . '/api';
+        $this->user =  User::factory()->make();
+    }
 
-        //Content::factory()->count(100)->create();
+    public function provideStoreCreatesNewContentData(): array
+    {
+        return [
+            'Input' =>
+                [
+                    [
+                        'title' => 'officias',
+                        'release_date' => '1993-01-09',
+                        'content_type' => 'short_film',
+                        'genre' => json_encode(['genre1' => 'horror', 'genre2' => 'splatter']),
+                        'tags' => json_encode(['tag1' => 'horror', 'tag2' => 'splatter']),
+                        'runtime' => 149,
+                        'short_description' => 'Aliquam sint provident repellendus aspernatur. Impedit et molestiae fugiat. Laudantium fuga deleniti dolor maxime earum. In ullam quia omnis asperiores.',
+                        'cast' => json_encode(['starring1' => 'Fin Biessler', 'starring2' => 'Luis Platzer']),
+                        'directors' => json_encode(['director1' => 'Fin Biessler', 'director2' => 'Luis Platzer']),
+                        'age_restriction' => '12',
+                        'poster_url' => 'http://morar.info/iusto-non-quae-voluptate-amet-inventore',
+                        'youtube_trailer_url' => 'http://wehner.org/minus-doloremque-qui-autem-officia-enim',
+                        'production_company' => 'Hand-Wolff',
+                        'seasons' => 15,
+                        'average_episode_count' => 13,
+                    ]
+                ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideStoreCreatesNewContentData
+     */
+    public function testStoreCreatesNewContent(array $input): void
+    {
+        $this->actingAs($this->user)
+            ->postJson(
+                '/api/content',
+                $input
+            )->assertJson([
+                'status' => 'success',
+                'content_created' => $input,
+                'message' => 'Successfully created a new content entry'
+            ]);
     }
 
     public function testIndexReturnsAllContents(): void
     {
+        Content::factory()->count(100)->create();
 
+        $responseData = $this->actingAs($this->user)
+        ->getJson(
+            '/api/content'
+        )->json('data');
+
+        self::assertEquals(100, sizeof($responseData));
     }
 }
