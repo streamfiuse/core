0670725f9b97c2cddfa91d6e50e810e41fbc2796
finbiessler <fin.biess@gmx.de>
2021-02-19 20:38:22 +0100

    Outsourced content request validation rules to separate classes

---
+++ app/Http/Requests/ContentUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContentUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'string|unique:content,title',
+            'release_date' => 'date_format:Y-m-d',
+            'content_type' => 'string',
+            'genre' => 'json',
+            'tags' => 'json',
+            'runtime' => 'integer|gt:0',
+            'short_description' => 'string',
+            'cast' => 'json',
+            'directors' => 'json',
+            'age_restriction' => 'string',
+            'poster_url' => 'url',
+            'youtube_trailer_url' => 'url',
+            'production_company' => 'string',
+            'seasons' => 'integer|gt:0',
+            'average_episode_count' => 'integer|gt:0',
+        ];
+    }
+}
