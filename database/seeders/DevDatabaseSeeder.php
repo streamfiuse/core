1475e9c6bb94ff640cc06b82bb819237d098bc3f
finbiessler <fin.biess@gmx.de>
2021-03-08 12:56:21 +0100

    DevDatabaseSeeder now seed database with 4 users and related (liked contents)

--- database/seeders/DevDatabaseSeeder.php
+++ database/seeders/DevDatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Content;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DevDatabaseSeeder extends Seeder
 {
@@ -13,9 +17,18 @@ class DevDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            ContentSeeder::class,
-            UserSeeder::class
-        ]);
+        User::factory()->count(4)
+            ->hasAttached(
+                Content::factory()->count(10),
+                [
+                    'position' => 1,
+                    'like_status' => 'liked',
+                    'created_at' => Carbon::now()
+                ]
+            )
+            ->create();
+        for ($i = 2; $i < 41; $i++) {
+            DB::table('content_user')->where('content_id', '=',  $i)->update(['position' => $i%10 !== 0 ? $i%10 : 10]);
+        }
     }
 }
