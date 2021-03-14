862d5da9f12b861dd36b801b579901a33f2aee2d
finbiessler <fin.biess@gmx.de>
2021-03-14 15:42:46 +0100

    added testing account to dev database seeder

--- database/seeders/DevDatabaseSeeder.php
+++ database/seeders/DevDatabaseSeeder.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Seeder;
 use App\Models\Content;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DevDatabaseSeeder extends Seeder
 {
@@ -27,8 +30,19 @@ class DevDatabaseSeeder extends Seeder
                 ]
             )
             ->create();
+
+        DB::table('users')->insert([
+            'name' => 'tester',
+            'email' => 'tester@email.com',
+            'email_verified_at' => Date::now(),
+            'password' => Hash::make('testerpassword'),
+            'remember_token' => Str::random(12),
+            'created_at' => Date::now()
+        ]);
+
         for ($i = 2; $i < 41; $i++) {
             DB::table('content_user')->where('content_id', '=',  $i)->update(['position' => $i%10 !== 0 ? $i%10 : 10]);
+            DB::table('content_user')->where('content_id', '=',  $i)->update(['like_status' => $i%2 !== 0 ? 'liked' : 'disliked']);
         }
     }
 }
