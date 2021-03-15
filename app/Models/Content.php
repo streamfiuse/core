67e5cba192af8aef95a0e0531c34160946bedc11
finbiessler <fin.biess@gmx.de>
2021-03-15 21:58:28 +0100

    * created FiuselistRepository * cs fix

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -1,16 +1,10 @@
 <?php
 
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-/**
- * @mixin Builder
- */
 class Content extends Model
 {
     use HasFactory;
---
+++ app/Repositories/Fiuselist/FiuselistRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Repositories\Fiuselist;
+
+
+use App\Repositories\QueryBaseRepository;
+
+class FiuselistRepository extends QueryBaseRepository implements FiuselistRepositoryInterface
+{
+
+}
