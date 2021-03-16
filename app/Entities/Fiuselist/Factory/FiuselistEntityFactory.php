1982956b2e5670ebf6d4aac4b65d8f636f74662b
finbiessler <fin.biess@gmx.de>
2021-03-16 15:54:43 +0100

    bootstrapped dbal for fiuselist

---
+++ app/Entities/Fiuselist/Factory/FiuselistEntityFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Entities\Fiuselist\Factory;
+
+
+use App\Entities\Fiuselist\FiuselistEntity;
+
+class FiuselistEntityFactory
+{
+    private FiuselistEntryEntityFactory $fiuselistEntryEntityFactory;
+    public function __construct(FiuselistEntryEntityFactory $fiuselistEntryEntityFactory)
+    {
+        $this->fiuselistEntryEntityFactory = $fiuselistEntryEntityFactory;
+    }
+
+    public function create(object $fiuselistData): FiuselistEntity
+    {
+        $fiuselistEntries = [];
+        foreach ($fiuselistData as $fiuselistEntryData) {
+            $fiuselistEntries[] = $this->fiuselistEntryEntityFactory->create(json_decode(json_encode($fiuselistEntryData), true));
+        }
+        return new FiuselistEntity($fiuselistEntries);
+    }
+}
---
+++ app/Entities/Fiuselist/Factory/FiuselistEntryEntityFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Entities\Fiuselist\Factory;
+
+
+use App\Entities\Fiuselist\FiuselistEntryEntity;
+
+class FiuselistEntryEntityFactory
+{
+    public function create(
+        array $fiuselistEntryData
+    ): FiuselistEntryEntity
+    {
+        return new FiuselistEntryEntity(
+            $fiuselistEntryData['content_id'],
+            $fiuselistEntryData['user_id'],
+            $fiuselistEntryData['position'],
+            $fiuselistEntryData['like_status'],
+            $fiuselistEntryData['dislike_count'],
+            $fiuselistEntryData['free_date'],
+            $fiuselistEntryData['created_at'],
+            $fiuselistEntryData['updated_at'],
+        );
+    }
+}
