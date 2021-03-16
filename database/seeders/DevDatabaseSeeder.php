0e5ed8d10c3c79892593ac3fa0f8e201cef46762
finbiessler <fin.biess@gmx.de>
2021-03-16 16:32:46 +0100

    refactored database seeding

--- database/seeders/DevDatabaseSeeder.php
+++ database/seeders/DevDatabaseSeeder.php
@@ -20,6 +20,7 @@ class DevDatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Content and user plus relation seeding
         User::factory()->count(4)
             ->hasAttached(
                 Content::factory()->count(10),
@@ -31,18 +32,22 @@ class DevDatabaseSeeder extends Seeder
             )
             ->create();
 
-        DB::table('users')->insert([
-            'name' => 'tester',
-            'email' => 'tester@email.com',
-            'email_verified_at' => Date::now(),
-            'password' => Hash::make('testerpassword'),
-            'remember_token' => Str::random(12),
-            'created_at' => Date::now()
-        ]);
-
         for ($i = 2; $i < 41; $i++) {
             DB::table('content_user')->where('content_id', '=',  $i)->update(['position' => $i%10 !== 0 ? $i%10 : 10]);
             DB::table('content_user')->where('content_id', '=',  $i)->update(['like_status' => $i%2 !== 0 ? 'liked' : 'disliked']);
+        }
+
+        // Test account seeding
+        $isTesterAlreadySeeded = boolval(DB::table('users')->where('email', '=', 'tester@email.com')->count());
+        if ($isTesterAlreadySeeded === false) {
+            DB::table('users')->insert([
+                'name' => 'tester',
+                'email' => 'tester@email.com',
+                'email_verified_at' => Date::now(),
+                'password' => Hash::make('testerpassword'),
+                'remember_token' => Str::random(12),
+                'created_at' => Date::now()
+            ]);
         }
     }
 }
