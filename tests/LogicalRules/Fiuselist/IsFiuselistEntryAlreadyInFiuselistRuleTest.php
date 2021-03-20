00cf588d726b7499ce1b8496d402bc2f07cd8fed
finbiessler <fin.biess@gmx.de>
2021-03-20 19:40:53 +0100

    bootstrapped new testsuite for logical rules

---
+++ tests/LogicalRules/Fiuselist/IsFiuselistEntryAlreadyInFiuselistRuleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\LogicalRules\Fiuselist;
+
+use App\LogicalRules\Fiuselist\IsFiuselistEntryAlreadyInFiuselistRule;
+use PHPUnit\Framework\TestCase;
+
+class IsFiuselistEntryAlreadyInFiuselistRuleTest extends TestCase
+{
+
+}
