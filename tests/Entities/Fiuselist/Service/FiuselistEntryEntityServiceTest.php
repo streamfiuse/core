9b0493d74d04978f5ccbb06105693d610c68c5dc
finbiessler <fin.biess@gmx.de>
2021-03-20 14:13:59 +0100

    bootstrapped testsuite for entity tests

---
+++ tests/Entities/Fiuselist/Service/FiuselistEntryEntityServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Entities\Fiuselist\Service;
+
+use App\Entities\Fiuselist\Service\FiuselistEntryEntityService;
+use PHPUnit\Framework\TestCase;
+
+class FiuselistEntryEntityServiceTest extends TestCase
+{
+    public function testTestCase(): void
+    {
+        self::assertSame(true, false);
+    }
+}
