0e1c723974db35f94e8f4bad28366be739b49033
finbiessler <fin.biess@gmx.de>
2021-03-20 17:27:00 +0100

    implemented test for FiuselistEntryEntityService

--- tests/Entities/Fiuselist/Service/FiuselistEntryEntityServiceTest.php
+++ tests/Entities/Fiuselist/Service/FiuselistEntryEntityServiceTest.php
@@ -2,13 +2,113 @@
 
 namespace Tests\Entities\Fiuselist\Service;
 
+use App\Entities\Fiuselist\Factory\FiuselistEntryEntityFactory;
+use App\Entities\Fiuselist\FiuselistEntryEntity;
 use App\Entities\Fiuselist\Service\FiuselistEntryEntityService;
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 
 class FiuselistEntryEntityServiceTest extends TestCase
 {
-    public function testTestCase(): void
+    private FiuselistEntryEntityService $fiuselistEntryEntityService;
+
+    public function setUp(): void
     {
-        self::assertSame(true, false);
+        parent::setUp();
+        $this->fiuselistEntryEntityService = new FiuselistEntryEntityService(new FiuselistEntryEntityFactory());
+    }
+
+    public function provideFiuselistEntryEntityCreationData(): array
+    {
+        return [
+            [
+                [
+                    'content_id' => 10,
+                    'user_id' => 5,
+                    'position' => 2,
+                    'like_status' => 'liked'
+                ],
+                new FiuselistEntryEntity(
+                    10,
+                    5,
+                    2,
+                    'liked',
+                    0,
+                    null,
+                    Carbon::today()->toISOString(),
+                    null
+                )
+            ],
+            [
+                [
+                    'content_id' => 10,
+                    'user_id' => 5,
+                    'position' => 2,
+                    'like_status' => 'disliked'
+                ],
+                new FiuselistEntryEntity(
+                    10,
+                    5,
+                    2,
+                    'disliked',
+                    1,
+                    Carbon::today()->addDays(30)->toISOString(),
+                    Carbon::today()->toISOString(),
+                    null
+                )
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideFiuselistEntryEntityCreationData
+     */
+    public function testFiuselistEntryEntityCreationFromAttributesIsCorrect($attributes, $expected): void
+    {
+        $actual = $this->fiuselistEntryEntityService->createFiuselistEntryEntityFromAttributes(
+            $attributes['content_id'],
+            $attributes['user_id'],
+            $attributes['position'],
+            $attributes['like_status']
+        );
+
+        self::assertEquals($expected, $actual);
+    }
+
+    public function provideExtractionOfFiuselistEntryEntityWorksCorrectlyData(): array
+    {
+        return [
+            [
+                new FiuselistEntryEntity(
+                    10,
+                    5,
+                    2,
+                    'liked',
+                    0,
+                    null,
+                    Carbon::today()->toISOString(),
+                    null
+                ),
+                [
+                    'content_id' => 10,
+                    'user_id' => 5,
+                    'position' => 2,
+                    'like_status' => 'liked',
+                    'dislike_count' => 0,
+                    'free_date' => '',
+                    'created_at' => Carbon::today()->toISOString(),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideExtractionOfFiuselistEntryEntityWorksCorrectlyData
+     */
+    public function testExtractionOfFiuselistEntryEntityWorksCorrectly(FiuselistEntryEntity $fiuselistEntryEntity, array $expectedAttributes): void
+    {
+        $actual = $this->fiuselistEntryEntityService->extractAttributesArrayFromFiuselistEntryEntity($fiuselistEntryEntity);
+
+        self::assertEquals($expectedAttributes, $actual);
     }
 }
