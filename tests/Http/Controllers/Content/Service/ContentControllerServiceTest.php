eb66a3707d6df6785b25634877c96b0bc77aead1
finbiessler <fin.biess@gmx.de>
2021-03-23 09:07:18 +0100

    WIP

---
+++ tests/Http/Controllers/Content/Service/ContentControllerServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Http\Controllers\Content\Service;
+
+use App\Http\Controllers\Content\Service\ContentControllerService;
+use App\Models\Content;
+use PHPUnit\Framework\TestCase;
+
+
+class ContentControllerServiceTest extends TestCase
+{
+    private ContentControllerService $contentControllerService;
+    private const CONTENT_COUNT = 4;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->contentControllerService = new ContentControllerService();
+        Content::factory()->count(self::CONTENT_COUNT)->create();
+    }
+
+    public function provideIdentifiersAndRespectiveResult(): array
+    {
+        return [
+          'Existing identifiers' => [
+              [
+                  1,
+                  2
+              ],
+              'success'
+          ]
+        ];
+    }
+
+    /**
+     * @dataProvider provideIdentifiersAndRespectiveResult
+     */
+    public function testGetContentsByIdentifiers(array $identifiers, string $expected): void
+    {
+        $actual = $this->contentControllerService->getContentsByIdentifiers($identifiers);
+
+        self::assertEquals($actual['status'], $expected);
+    }
+}
